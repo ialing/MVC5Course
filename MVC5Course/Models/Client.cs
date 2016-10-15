6ba9f0bc150bdb4ace92c43979fc4462bb86790e
ialing <ialing.chang@gamil.com>
2016-10-15 14:30:14 +0800

    11 替 Client 模型類別的部分欄位加上一些 DisplayName 自訂顯示名稱

--- MVC5Course/Models/Client.cs
+++ MVC5Course/Models/Client.cs
@@ -11,6 +11,8 @@ namespace MVC5Course.Models
 {
     using System;
     using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.ComponentModel.DataAnnotations; 
     
     public partial class Client
     {
@@ -21,11 +23,25 @@ namespace MVC5Course.Models
         }
     
         public int ClientId { get; set; }
+        [Required]
+        [StringLength(10, ErrorMessage="{0}最大不得超過{1}個字元")]
+        [DisplayName("名")]
         public string FirstName { get; set; }
+        [Required]
+        [StringLength(10, ErrorMessage = "{0}最大不得超過{1}個字元")]
+        [DisplayName("中間名")]
         public string MiddleName { get; set; }
+        [Required]
+        [StringLength(5, ErrorMessage = "{0}最大不得超過{1}個字元")]
+        [DisplayName("姓氏")]
         public string LastName { get; set; }
+        [Required]
+        [RegularExpression("[MF]",ErrorMessage="{0}只能輸入Ｍ或Ｆ")]
+        [DisplayName("性別")]
         public string Gender { get; set; }
+        [DisplayName("生日")]
         public Nullable<System.DateTime> DateOfBirth { get; set; }
+        [DisplayName("信用評等")]
         public Nullable<double> CreditRating { get; set; }
         public string XCode { get; set; }
         public Nullable<int> OccupationId { get; set; }
