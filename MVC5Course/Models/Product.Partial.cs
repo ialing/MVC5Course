1b151ff8923ca9ae8a568a752a81e21abf372b7c
ialing <ialing.chang@gamil.com>
2016-10-16 17:34:34 +0800

    27 使用 EFGeneratePartialClass.tt 快速產生實體模型的 Metadata 類別，並實作 IValidableObject 模型驗證 (Model Valication)

---
+++ MVC5Course/Models/Product.Partial.cs
@@ -0,0 +1,39 @@
+namespace MVC5Course.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    
+    [MetadataType(typeof(ProductMetaData))]
+    public partial class Product : IValidatableObject
+    {
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (this.Price >= 1000 && this.Stock >= 1000)
+            {
+                yield return new ValidationResult("本商品價格與庫存不合理", new string[] { "Price", "Stock" });
+            }
+            if (this.ProductName == "will")
+            {
+                yield return new ValidationResult("為註冊商標", new string[] { "ProductName" });
+            }
+        }
+    }
+    
+    public partial class ProductMetaData
+    {
+        [Required]
+        public int ProductId { get; set; }
+        
+        [StringLength(80, ErrorMessage="欄位長度不得大於 80 個字元")]
+        public string ProductName { get; set; }
+        [Range(0,99999)]
+        public Nullable<decimal> Price { get; set; }
+        public Nullable<bool> Active { get; set; }
+        public Nullable<decimal> Stock { get; set; }
+        [Required]
+        public bool IsDeleted { get; set; }
+    
+        public virtual ICollection<OrderLine> OrderLine { get; set; }
+    }
+}
