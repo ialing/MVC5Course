2170673aa82702ee7bd4c003fdb686c9edb21e88
ialing <ialing.chang@gamil.com>
2016-10-30 11:27:17 +0800

    48 練習用 Razor 精準控制輸出

--- MVC5Course/Models/ViewModels/ProductBathUpdatViewModel.cs
+++ MVC5Course/Models/ViewModels/ProductBathUpdatViewModel.cs
@@ -6,21 +6,14 @@ using System.Web;
 
 namespace MVC5Course.Models.ViewModels 
 {
-    public class ProductBathUpdatViewModel : IValidatableObject
+    public class ProductBathUpdatViewModel  
     {
         public int ProductId { get; set; }
         public string ProductName { get; set; }
         public Nullable<decimal> Price { get; set; }
         public Nullable<bool> Active { get; set; }
         public Nullable<decimal> Stock { get; set; }
-        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
-        {
-            if (this.Price >= 1000 && this.Stock >= 1000)
-            {
-                yield return new ValidationResult("本商品價格與庫存不合理", new string[] { "Price", "Stock" });
-            }
-            
-        }
+   
     }
     
 }
